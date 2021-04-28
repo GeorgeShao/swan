ece551163bc71e1babc1d28c2a9a78aad7c5aa64
George Shao <georgeshao123@gmail.com>
2021-04-27 22:28:19 -0400

    🔧 Setup Chakra UI theme and added nested Storybook.js component example

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Button from './Button'
+// import Center from '../Center/Center'
 
 export default {
-    title: 'Button',
-    component: Button
+    title: 'Form/Button',
+    component: Button,
+    args: {
+        children: 'Button',
+    },
+    // decorators: [story => <Center>{story()}</Center>]
 }
 
 export const Primary = () => <Button variant='primary'>Primary</Button>
@@ -14,3 +19,23 @@ export const Danger = () => <Button variant='danger'>Danger</Button>
 export const Small = () => <Button variant='primary' size='small'>Small</Button>
 export const Medium = () => <Button variant='primary' size='medium'>Medium</Button>
 export const Large = () => <Button variant='primary' size='large'>Large</Button>
+
+const Template = args => <Button {...args} />
+
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+    variant: 'primary',
+    // children: 'Primary Args'
+}
+
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args = {
+    ...PrimaryA.args,
+    // children: 'Long Primary Args'
+}
+
+export const SecondaryA = Template.bind({})
+SecondaryA.args = {
+    variant: 'secondary',
+    // children: 'Secondary Args'
+}
